d98ce355fceca48bab0d827e9ca20fceea7775e5
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2024-09-20 20:50:33 +0700

    divided class into header and .cpp

--- NSU_2nd_COURSE/file_processing.cpp
+++ NSU_2nd_COURSE/file_processing.cpp
@@ -6,33 +6,37 @@
 
 file_processing::file_processing(const std::string& filename) : filename_(filename) {}
 
-void file_processing::extract_from_txt() {
+void file_processing::extract_from_txt() 
+{
     std::ifstream file(filename_);
-    if (!file.is_open()) {
+    if (!file.is_open()) 
+    {
         std::cerr << "cannot open TXT file: " << filename_ << std::endl;
         return;
     }
 
     std::string line;
-    while (std::getline(file, line)) {
-
+    while (std::getline(file, line)) 
+    {
         std::istringstream iss(line);
         std::string word;
 
         //split the string by delimeter chars
-        while (iss >> word) {
+        while (iss >> word) 
+        {
             std::string clean_word;
-            for (char c : word) {
-
-                if (std::isalnum(c)) {
+            for (char c : word) 
+            {
+                if (std::isalnum(c)) 
+                {
                     clean_word += c;
-                } else if (!clean_word.empty()) {
-
+                } 
+                else if (!clean_word.empty()) 
+                {
                     words_map[clean_word]++;
                     words_counter++;
                     clean_word.clear(); 
-            }
-
+                }
             if (!clean_word.empty()) {
                 words_map[clean_word]++;
                 words_counter++;
@@ -42,13 +46,15 @@ void file_processing::extract_from_txt() {
     file.close();
 
     //convert data into a vector and calculate frequency as a percentage
-    for (const auto& pair : words_map) {
+    for (const auto& pair : words_map) 
+    {
         float frequency_percent = (static_cast<float>(pair.second) / words_counter) * 100;
         data_.emplace_back(pair.first, pair.second, frequency_percent);
     }
 
     //sort data in descending order of frequencies as a percentage 
-    std::sort(data_.begin(), data_.end(), [](const auto& a, const auto& b) {
+    std::sort(data_.begin(), data_.end(), [](const auto& a, const auto& b) 
+    {
         return std::get<2>(a) > std::get<2>(b); // Сортируем по процентной частоте
     });
 }
