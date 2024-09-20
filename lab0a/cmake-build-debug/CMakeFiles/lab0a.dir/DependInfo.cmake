f339f18fdc0d422e6ed46533457eeb34a77812a3
Ivan Sharapov <151162152+shh1vachan@users.noreply.github.com>
2024-09-20 21:41:42 +0700

    Add files via upload

---
+++ lab0a/cmake-build-debug/CMakeFiles/lab0a.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/shh1vachan/Документы/c++/lab0a/main.cpp" "CMakeFiles/lab0a.dir/main.cpp.o" "gcc" "CMakeFiles/lab0a.dir/main.cpp.o.d"
+  "/home/shh1vachan/Документы/c++/lab0a/module1.cpp" "CMakeFiles/lab0a.dir/module1.cpp.o" "gcc" "CMakeFiles/lab0a.dir/module1.cpp.o.d"
+  "/home/shh1vachan/Документы/c++/lab0a/module2.cpp" "CMakeFiles/lab0a.dir/module2.cpp.o" "gcc" "CMakeFiles/lab0a.dir/module2.cpp.o.d"
+  "/home/shh1vachan/Документы/c++/lab0a/module3.cpp" "CMakeFiles/lab0a.dir/module3.cpp.o" "gcc" "CMakeFiles/lab0a.dir/module3.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab0a/module3.cpp
@@ -0,0 +1,11 @@
+#include "module3.h"
+
+//new namespace
+namespace Module3
+{
+    std::string getMyName()
+    {
+        std::string name = "Peter";
+        return name;
+    }
+}
